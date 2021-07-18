af1275dd8126efb30be44bb5918b6305ccf5a557
Nick Norman <nicknorm007@gmail.com>
2021-07-18 10:24:34 -0400

    add page query

--- precips.js
+++ precips.js
@@ -8,22 +8,39 @@ const start_date = "#frmPrecipReportSearch_ucDateRangeFilter_dcStartDate_t"
 const end_date = "#frmPrecipReportSearch_ucDateRangeFilter_dcEndDate_t"
 const wilmington_station = "MA-MD-85"
 
+function run () {
 
-async function run () {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.type(station_field, wilmington_station);
-  await page.click(station_number_chkbox)
+  return new Promise(async (resolve, reject) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-
-  await page.click(search_btn)
-  browser.close();
+        //fill in form
+        await page.goto(url);
+        await page.type(station_field, wilmington_station);
+        await page.click(station_number_chkbox)
+        await page.type(start_date, "06/30/2021");
+        await page.type(end_date, "07/18/2021");
+        await Promise.all([page.waitForNavigation(), page.click(search_btn)]);
+        
+        let totals = await page.evaluate(() => {
+          const all_precips_selector = "tr[class$='Item'] > td:nth-child(5)"
+          let results = [];
+          let items_all = document.querySelectorAll(all_precips_selector);
+          items_all.forEach((item) => {
+            let t = item.innerText.trim()
+            results.push({
+              total: t,
+            });
+          });
+          return results;
+        })
+        browser.close();
+        return resolve(totals);
+    } catch (e) {
+        return reject(e);
+    }
+ })
 }
 
-function logit() {
-
-  console.log("")
-}
-
-run();
+run().then(console.log).catch(console.error);
