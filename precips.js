87db482fbe97bd3b90325b9fad3a870cb7269065
Nick Norman <nicknorm007@gmail.com>
2021-09-23 16:59:39 -0400

    streaks

--- precips.js
+++ precips.js
@@ -35,7 +35,7 @@ function run () {
         const dropdowns = await page.$$eval("select" + select_pager + " option", all => all.map(a => a.textContent))
         let num_pages = dropdowns == 0 ? 1 : dropdowns.length;
         let grand_totals = 0;
-        let full_data = { grand_totals: 0.0, resultsTotal: new Array() };
+        let full_data = { grand_totals: 0.0, resultsTotal: new Array(), precip_dates: new Array() };
         
         for(let pgs=1; pgs<=num_pages; pgs++)
         {
@@ -50,9 +50,30 @@ function run () {
             
             let total_sums = (acc, current) => acc + current;
             let all_precips_selector = "tr[class$='Item'] > td:nth-child(5)"
+            let precips_date_selector = "tr[class$='Item'] > td:nth-child(1)"
             let items_all = document.querySelectorAll(all_precips_selector);
             let results_data = {};
             let results = [];
+            let item_rows = "tr[class$='Item']"
+            let item_row_selectors = document.querySelectorAll(item_rows);
+            let itemsresults = [];
+            let dates_precips = {};
+            
+            item_row_selectors.forEach((item) => {
+              let row = item.innerText.trim();
+              let parts = row.split('\t');
+              parts.forEach((part, idx) => {
+                if(idx == 0)
+                {
+                  dates_precips.pDate = part.trim();
+                }
+                if(idx == 4)
+                {
+                  dates_precips.total = part.trim();
+                }
+              });
+              itemsresults.push(Object.assign({}, dates_precips));
+            });
             items_all.forEach((item) => {
               let t = item.innerText.trim();
               if(t !== "T"){
@@ -61,8 +82,10 @@ function run () {
             });
             results_data.results = results;
             results_data.data = results.reduce(total_sums).toFixed( 2 );
+            results_data.dates = itemsresults;
             return results_data;
           }) //end page
+          full_data.precip_dates = full_data.precip_dates.concat(totals.dates);
           full_data.grand_totals = parseFloat(full_data.grand_totals) + parseFloat(totals.data);
           full_data['resultsTotal'].push(totals.results)
         //end
@@ -77,9 +100,38 @@ function run () {
  })
 }
 
+const calculateZeroPrecipStreak = (data) => {
+  let i,
+    temp,
+    streak,
+    length,
+    maxStreak = 0;
+
+    console.log(data)
+
+    //extract the total from data
+    let arr = data.map(d => d.total);
+    length = arr.length;
+    
+    for(i = 0; i < length; i++) {
+        if(temp !== '' && temp == arr[i] && temp == '0.00') {
+            streak++;
+        } else {
+            streak = 1;
+        }
+        temp = arr[i];
+        if(streak > maxStreak) {
+            maxStreak = streak;
+        }
+    }
+    console.log("Longest streak without precip: " + maxStreak + " days.");
+    return maxStreak;
+}
+
 run().then((value) => {
+
+  calculateZeroPrecipStreak(value.precip_dates);
   console.log(value.grand_totals + " inches");
-  console.log(value.resultsTotal.flat());
   console.log("Start date was " + args[2])
   console.log("End date was " + args[3])
   console.log("Station was " + (args[4] || wx_station))
